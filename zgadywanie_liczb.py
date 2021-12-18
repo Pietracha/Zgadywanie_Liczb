8d99de69e8384310f1238e26582b942755fbcee9
Piotr Milcarz <milcarz.piotr@gmail.com>
2021-12-18 19:48:42 +0100

    Zgadywanie_Liczb

---
+++ zgadywanie_liczb.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+def draw():
+    """
+    Function asks the user to provide a number and checks if it is an integer value. Repeats the request if it is not.
+    :rtype: int
+    :return: number provided by the user as int
+    """
+    while True:
+        try:
+            number_1 = int(input("Guess the number: "))
+            break
+        except ValueError:
+            print("It's not a number!")
+    return number_1
+
+
+def check():
+    """
+    Function checks if number provided by user matches the one drawn by computer
+    """
+    number_2 = randint(1,10)
+    get_number = draw()
+    while get_number != number_2:
+        if get_number < number_2:
+            print("Too small!")
+        else:
+            print("Too big!")
+        get_number = draw()
+    print("You win!")
+
+
+check()
